9ce5986cf0938c01b87a26931f20c076eed6f35b
ahmedoualha <ahmed@acteol.com>
2017-06-06 20:21:14 +0100

    added server.html

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
+    fs = require('fs'),
     app     = express();
     
-var port = 8080,
-    ip   = '0.0.0.0';
+var port = 8080;
 
-app.listen(port, ip);
+app.listen(port, function () {
+    console.log('Server running on http://%s:%s', ip, port);
+});
+
+app.get('/', function(request, response) {
+    fs.createReadStream('./views/server.html').pipe(response);
+});
+
+app.get('/main', function(request, response) {
+    fs.createReadStream('./views/index.html').pipe(response);
+});
+
 
 console.log('Server running on http://%s:%s', ip, port);
