810425fc006f03c3289c88db63e1da2b5d68b305
ahmedoualha <ahmed@acteol.com>
2017-06-06 19:34:39 +0100

    cleaned

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ var express = require('express'),
     fs      = require('fs'),
     app     = express();
     
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+var port = 8080,
+    ip   = '0.0.0.0';
 
 app.listen(port, ip);
 
 console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
