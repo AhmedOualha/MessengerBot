92a1ef676b4049f01f11f7ba07e05dce487908c5
ahmedoualha <ahmed@acteol.com>
2017-06-07 10:05:13 +0100

    port change

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express = require('express'),
     fs = require('fs'),
     app     = express();
     
-var port = 8080;
+var port = 80;
 
 app.listen(port, function () {
-    console.log('Server running on %s', process.domain);
+    console.log('Server running on https://0.0.0.0:%s', port);
 });
 
 app.get('/', function(request, response) {
@@ -17,3 +17,12 @@ app.get('/main', function(request, response) {
     fs.createReadStream('./views/index.html').pipe(response);
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
