b7bbbf24e139680146655eb4e5397980e67d3b97
ahmedoualha <ahmed@acteol.com>
2017-06-08 17:44:19 +0100

    change get link

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/main', function(request, response) {
     fs.createReadStream('./views/index.html').pipe(response);
 });
 
-app.get('/.well-known/acme-challenge/LXKi-T2TmK5VpXonEmk9aMPnkfdp0OJ4fOU7goiHyv4', function(request, response) {
-    fs.createReadStream('./.well-known/acme-challenge/LXKi-T2TmK5VpXonEmk9aMPnkfdp0OJ4fOU7goiHyv4').pipe(response);
+app.get('/.well-known/acme-challenge/lDqt9z1MkJqQr-lb5l3gd0IE1HrfEvC81on290LkC1Q', function(request, response) {
+    fs.createReadStream('./.well-known/acme-challenge/lDqt9z1MkJqQr-lb5l3gd0IE1HrfEvC81on290LkC1Q').pipe(response);
 });
 
 app.get('/.well-known/acme-challenge/ZdRobrw-KRYnJrGH6VDVzFB9Lfb0FKgbbgZoQ1IdF2w', function(request, response) {
