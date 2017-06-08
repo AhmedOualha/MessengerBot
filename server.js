7137742a9102a70f27b9828490bb0f3e2f2d701c
ahmedoualha <ahmed@acteol.com>
2017-06-08 17:39:57 +0100

    changes to openshift

--- server.js
+++ server.js
@@ -16,3 +16,11 @@ app.get('/', function(request, response) {
 app.get('/main', function(request, response) {
     fs.createReadStream('./views/index.html').pipe(response);
 });
+
+app.get('/.well-known/acme-challenge/LXKi-T2TmK5VpXonEmk9aMPnkfdp0OJ4fOU7goiHyv4', function(request, response) {
+    fs.createReadStream('./.well-known/acme-challenge/LXKi-T2TmK5VpXonEmk9aMPnkfdp0OJ4fOU7goiHyv4').pipe(response);
+});
+
+app.get('/.well-known/acme-challenge/ZdRobrw-KRYnJrGH6VDVzFB9Lfb0FKgbbgZoQ1IdF2w', function(request, response) {
+    fs.createReadStream('./.well-known/acme-challenge/ZdRobrw-KRYnJrGH6VDVzFB9Lfb0FKgbbgZoQ1IdF2w').pipe(response);
+});
