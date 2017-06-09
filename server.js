0e888db5388c7c1f651ab64c69c3b26425f091b5
ahmedoualha <ahmed@acteol.com>
2017-06-09 06:51:26 +0100

    heroku config

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express'),
     fs = require('fs'),
     app     = express();
     
-var port = 8080     ;
+var port = process.env.port || 8080;
+
+app.use(express.static(__dirname));
 
 app.listen(port, function () {
     console.log('Server running on https://0.0.0.0:%s', port);
@@ -17,6 +19,3 @@ app.get('/main', function(request, response) {
     fs.createReadStream('./views/index.html').pipe(response);
 });
 
-app.get('/.well-known/acme-challenge/VVpPX8fSHJiTJmiOzaMgxWxlESTeVm50MiKGTvVYhtg', function(request, response) {
-    fs.createReadStream('./.well-known/acme-challenge/VVpPX8fSHJiTJmiOzaMgxWxlESTeVm50MiKGTvVYhtg').pipe(response);
-});
